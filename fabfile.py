f874d8f15972c25c172b8aeda667d73a620108aa
dhilipsiva <dhilipsiva@pm.me>
2019-01-28 13:39:49 +0530

    More updates

--- fabfile.py
+++ fabfile.py
@@ -7,11 +7,14 @@ from os.path import dirname, realpath, exists as lexists
 from fabric.state import env
 from fabric.contrib.files import exists
 from fabric.colors import blue, green, red, yellow, cyan
-from fabric.api import cd, run, puts, task, lcd, local, abort, sudo
+from fabric.api import (
+    cd, run, puts, task, lcd, local, abort, sudo, hide, settings)
 
 import requests
-from fabtools import vagrant as _vagrant
+from fabtools.files import watch
+from fabtools.utils import run_as_root
 from fabtools import user, require, supervisor, nodejs
+
 
 DATA_FILE = '.infra.json'
 SSH_USERS = ['rs-ds']
@@ -42,12 +45,44 @@ class EnvNotSetup(Exception):
     pass
 
 
-def setup_env(app):
+@task
+def list_apps():
+    apps = []
+    for item in listdir(env.projects_path):
+        infra_file = join(env.projects_path, item, DATA_FILE)
+        if lexists(infra_file):
+            apps.append(item)
+    info("Available Apps: %s" % apps)
+
+
+def process_status(name):
+    """
+    Get the status of a supervisor process.
+    """
+    with settings(
+            hide('running', 'stdout', 'stderr', 'warnings'), warn_only=True):
+        res = run_as_root(
+            "supervisorctl --no-pager status %(name)s" % locals())
+        if res.startswith("No such process"):
+            return None
+        else:
+            return res.split()[1]
+
+
+def setup_env():
     '''
     Setup environment
     '''
-    env.app = app
-    env.app_user = app
+    if not (hasattr(env, 'app') and hasattr(env, 'environment')):
+        raise EnvNotSetup("Please specify app and environment")
+        abort("")
+    env.app_path_local = join(env.projects_path, env.app)
+    env.infra_file = join(env.app_path_local, DATA_FILE)
+    data = get_infra_data()
+    host = data["hosts"][env.environment]
+    env.user = host['user']
+    env.app_user = env.user
+    env.host_string = '%s@%s' % (host['user'], host['host'])
     env.home_path = user.home_directory(env.user)
     env.apps_path = join(env.home_path, 'apps')
     env.logs_path = join(env.home_path, 'logs')
@@ -56,30 +91,26 @@ def setup_env(app):
     env.app_path = join(env.apps_path, env.app)
     env.log_path = join(env.logs_path, env.app)
     env.env_path = join(env.envs_path, env.app)
-    env.app_path_local = join(env.projects_path, env.app)
-    env.infra_file = join(env.app_path_local, DATA_FILE)
-
-
-@task
-def get_apps():
-    apps = []
-    for item in listdir(env.projects_path):
-        infra_file = join(env.projects_path, item, DATA_FILE)
-        if lexists(infra_file):
-            apps.append(item)
-    return apps
 
 
 @task
 def vagrant(app=None):
-    _vagrant.vagrant()
     env.environment = 'dev'
-    if app is None:
-        apps = get_apps()
-        info("Usage: fab vagrant:<app> deploy")
-        info("Available Apps: %s" % apps)
-        abort("Please specify an app to deploy!")
-    setup_env(app)
+
+
+@task
+def staging():
+    env.environment = 'stag'
+
+
+@task
+def transportsimple_server():
+    env.app = 'transportsimple_server'
+
+
+@task
+def transportsimple_web():
+    env.app = 'transportsimple_web'
 
 
 @task
@@ -92,16 +123,15 @@ def sync_auth_keys():
     ssh_dir = join(user.home_directory(env.user), '.ssh')
     require.files.directory(ssh_dir, mode='700')
     authorized_keys_filename = join(ssh_dir, 'authorized_keys')
-    require.files.file(
-        authorized_keys_filename, mode='600')
+    require.files.file(authorized_keys_filename, mode='600')
     run('cat /dev/null > %s' % quote(authorized_keys_filename))
+    info("Fetching public keys from GitHub")
     for gh_user in SSH_USERS:
-        info("Fetching public keys from GitHub")
         r = requests.get("https://api.github.com/users/%s/keys" % gh_user)
         for key in r.json():
             run("echo %s >> %s"
                 % (quote(key["key"]), quote(authorized_keys_filename)))
-        success("Public keys synced")
+    success("Public keys synced")
 
 
 def git_head_rev():
@@ -160,6 +190,20 @@ def read_environment():
     return ",".join(open(env_file).readlines())
 
 
+def supervisor_process(name, **kwargs):
+    lines = []
+    params = {}
+    params.update(kwargs)
+    params.setdefault('autorestart', 'true')
+    params.setdefault('redirect_stderr', 'true')
+    lines.append('[program:%(name)s]' % locals())
+    for key, value in sorted(params.items()):
+        lines.append("%s=%s" % (key, value))
+    filename = '/etc/supervisor/conf.d/%(name)s.conf' % locals()
+    with watch(filename, callback=supervisor.update_config, use_sudo=True):
+        require.file(filename, contents='\n'.join(lines), use_sudo=True)
+
+
 def setup_service_django(service):
     args = service['args']
     _name = service['name']
@@ -167,7 +211,7 @@ def setup_service_django(service):
     command = "%s run gunicorn -c guniconfig.py %s %s" % (
         env.pipenv_path, args['wsgi_app'], args['port'])
     supervisor_log = join(env.log_path, _name + '_supervisor.log')
-    require.supervisor.process(
+    supervisor_process(
         service_name,
         command=command,
         directory=env.app_path,
@@ -187,36 +231,37 @@ def setup_service_angular(service):
     args = service['args']
     _name = service['name']
     service_name = "%s_%s" % (env.app, _name)
-    command = "ng build"
+    command = "ng build --prod --output-path dist"
     supervisor_log = join(env.log_path, _name + '_supervisor.log')
-    require.supervisor.process(
+    supervisor_process(
         service_name, command=command, directory=env.app_path,
         environment=read_environment(), user=env.user, startretries=1,
         stdout_logfile=supervisor_log, startsecs=0, autorestart=False,
-        # user=env.app_user,
     )
     for domain in args['domains']:
         CONFIG_TPL = '''
             server {
                 listen      80;
                 server_name %(server_name)s;
-                root        %(docroot)s;
+                root        %(docroot)s/dist;
                 access_log  /var/log/nginx/%(server_name)s.log;
             }'''
         require.nginx.site(
             domain, template_contents=CONFIG_TPL, docroot=env.app_path)
 
 
-setup_service = {
-    "django": setup_service_django,
-    "angular": setup_service_angular,
-}
+def setup_service(framework, service):
+    _setup_service = {
+        "django": setup_service_django,
+        "angular": setup_service_angular,
+    }.get(framework)
+    return _setup_service(service)
 
 
 def setup_services(data):
     for service in data['services']:
         framework = service['framework']
-        setup_service[framework](service)
+        setup_service(framework, service)
 
 
 def ensure_deps_python():
@@ -231,50 +276,57 @@ def ensure_deps_node():
         nodejs.install_package(package)
 
 
-ensure_deps = {
-    'python': ensure_deps_python,
-    'node': ensure_deps_node,
-}
+def ensure_deps(language):
+    _ensure_deps = {
+        'python': ensure_deps_python,
+        'node': ensure_deps_node,
+    }.get(language)
+    return _ensure_deps()
 
 
 def ensure_packages_python():
-    run("pipenv install")
-    run("pipenv run ./manage.py migrate")
+    run("%s install -d" % env.pipenv_path)
+    run("%s run ./manage.py migrate" % env.pipenv_path)
 
 
 def ensure_packages_node():
     nodejs.install_dependencies(npm='yarn')
 
 
-ensure_packages = {
-    'python': ensure_packages_python,
-    'node': ensure_packages_node,
-}
+def ensure_packages(language):
+    _ensure_packages = {
+        'python': ensure_packages_python,
+        'node': ensure_packages_node,
+    }.get(language)
+    return _ensure_packages()
 
 
 @task
 def setup():
-    if not hasattr(env, 'app'):
-        raise EnvNotSetup("Please setup the env (setup_env)")
+    info('Starting Deployment for %s in %s' % (env.app, env.host_string))
+    setup_env()
     data = get_infra_data()
     language = data['language']
     info('Starting Deployment for %s in %s' % (env.app, env.host_string))
     require.deb.uptodate_index()
-    ensure_deps[language]()
+    require.deb.packages(['supervisor'])
+    ensure_deps(language)
     require.users.user(env.app_user, system=True)
     require.files.directories([env.app_path, env.log_path, env.env_path])
     git_push()
     with cd(env.app_path):
-        ensure_packages[language]()
+        ensure_packages(language)
     setup_services(data)
 
 
 @task
 def deploy():
+    setup_env()
     git_push()
     supervisor.reload_config()
 
 
 @task
-def echo_test():
-    _vagrant.vagrant()
+def ping():
+    setup_env()
+    run("echo pong")
